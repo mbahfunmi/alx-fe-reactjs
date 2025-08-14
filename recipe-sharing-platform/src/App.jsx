99ff2ed87adcff448f0978fd8009d6073def3c6a
mbahfunmi <mbahfunmi@gmail.com>
2025-08-14 10:18:16 +0100

    recipe-sharing platform and all its content

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,9 @@
+// src/App.jsx
+function App() {
+  return (
+    <div className="text-center p-4">
+      <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind!</h1>
+    </div>
+  );
+}
+export default App;
